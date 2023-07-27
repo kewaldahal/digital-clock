f5f35acf0667095eddcebc3495cc6b5506efbd44
Kewal <110725776+ProfDhlX@users.noreply.github.com>
2023-07-27 17:40:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function updateClock() {
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
-  
+
   document.getElementById('hours').innerText = hours;
   document.getElementById('minutes').innerText = minutes;
   document.getElementById('seconds').innerText = seconds;
@@ -13,8 +13,30 @@ function updateClock() {
   // Use the converted Nepali time as needed
 }
 
-// Update the clock every second
-setInterval(updateClock, 1000);
+function startTimer(duration) {
+  let timer = duration;
+  let minutes, seconds;
 
-// Initial call to avoid delay in the first update
+  const intervalId = setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      document.getElementById('timerDisplay').innerText = minutes + ':' + seconds;
+
+      if (--timer < 0) {
+          clearInterval(intervalId);
+          document.getElementById('timerDisplay').innerText = 'Time\'s up!';
+      }
+  }, 1000);
+}
+
+setInterval(updateClock, 1000);
 updateClock();
+
+document.getElementById('startTimerBtn').addEventListener('click', function () {
+  const selectedTime = parseInt(document.getElementById('timerDropdown').value);
+  startTimer(selectedTime);
+});
